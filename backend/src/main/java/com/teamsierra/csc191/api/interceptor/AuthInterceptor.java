7f2fbfd64e0e79de0ff37aff322730b42cc047a0
scottlivingston <s.livingst@gmail.com>
2013-10-18 18:35:58 -0700

    api now queries facebook with access token from header 'fbAccessToken' no vaidation occurs yet

--- backend/src/main/java/com/teamsierra/csc191/api/interceptor/AuthInterceptor.java
+++ backend/src/main/java/com/teamsierra/csc191/api/interceptor/AuthInterceptor.java
@@ -1,19 +1,16 @@
 package com.teamsierra.csc191.api.interceptor;
 
-import com.teamsierra.csc191.api.model.Appointment;
 import com.teamsierra.csc191.api.model.User;
 import com.teamsierra.csc191.api.repository.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
@@ -36,7 +33,27 @@ public class AuthInterceptor implements HandlerInterceptor{
                              HttpServletResponse response,
                              Object handler) {
 
-        RestTemplate restTemplate = new RestTemplate();
+        String id = request.getHeader("fbUserId");
+        User user = userRepository.findByOAuthId(id);
+
+
+        if (user != null) { //user exists
+            if (user.getToken().equals(request.getHeader("fbAccessToken"))) { //access token is good
+                return true;
+            } else { //access token is bad
+                //TODO validate the id returned is user.oauthId
+                facebookChallenge(request.getHeader("fbUserId"), request.getHeader("fbAccessToken"));
+            }
+        } else { //user does not exist
+            //TODO challenge header.fbAccessToken with facebook
+            facebookChallenge(request.getHeader("fbUserId"), request.getHeader("fbAccessToken"));
+            //TODO create user???
+        }
+
+        return true;
+    }
+
+    public boolean facebookChallenge(String id, String token) {
         Properties p = new Properties();
         try {
             p.load(this.getClass().getClassLoader().getResourceAsStream("system.properties"));
@@ -46,27 +63,25 @@ public class AuthInterceptor implements HandlerInterceptor{
             e.printStackTrace();
         }
 
-        String id = request.getHeader("fbUserId");
-        User user = userRepository.findByOAuthId(id);
+        RestTemplate restTemplate = new RestTemplate();
 
 
-        if (user != null) { //user exists
-            if (user.getToken().equals(request.getHeader("fbAccessToken"))) { //access token is good
-                return true;
-            } else { //access token is bad
-                Map<String, String> challengeVars = new HashMap<String, String>();
-                challengeVars.put("accessToken", request.getHeader("fbAccessToken"));
-                challengeVars.put("appSecret", p.getProperty("facebookSecret"));
-                //TODO challenge header.fbAccessToken with facebook
-                String apiUrl = "https://graph.facebook.com/debug_token?input_token={accessToken}&access_token={appSecret}";
-                String fbChallenge = restTemplate.getForObject(apiUrl, String.class, challengeVars);
-                L.info(fbChallenge);
-                //TODO validate the id returned is user.oauthId
-            }
-        } else { //user does not exist
-            //TODO challenge header.fbAccessToken with facebook
-            //TODO create user???
-        }
+        String appAccessUrl = "https://graph.facebook.com/oauth/access_token?client_id={id}&client_secret={secret}&grant_type=client_credentials";
+        Map<String, String> getAppAccessVars = new HashMap<>();
+        getAppAccessVars.put("id", "197300770451342");
+        getAppAccessVars.put("secret", p.getProperty("facebookSecret"));
+
+        String appAccess = restTemplate.getForObject(appAccessUrl, String.class, getAppAccessVars);
+        L.info(appAccess);
+        appAccess = appAccess.substring(appAccess.indexOf('=')+1);
+
+        String apiUrl = "https://graph.facebook.com/debug_token?input_token={accessToken}&access_token={appToken}";
+        Map<String, String> challengeVars = new HashMap<>();
+        challengeVars.put("accessToken", token);
+        challengeVars.put("appToken", appAccess);
+
+        String fbChallenge = restTemplate.getForObject(apiUrl, String.class, challengeVars);
+        L.info(fbChallenge);
 
         return true;
     }
