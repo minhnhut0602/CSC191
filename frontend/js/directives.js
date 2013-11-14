23c28dcdbd3714dd3f68fc2e5dfc84681408ab5d
scottlivingston <s.livingst@gmail.com>
2013-11-13 19:51:13 -0800

    made custom directive wrappers for bootstrap/jquery plugins.

--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -1,74 +1,140 @@
 var scheduleDirectives = angular.module('scheduleDirectives', []);
 
-scheduleDirectives.directive('initCalendar', function() {
-    return function(scope, element, attrs) {
-        var visiblePopover;
+// scheduleDirectives.directive('initCalendar', function() {
+//     return function(scope, element, attrs) {
+//         var visiblePopover;
 
-        // enable popovers - all of mine in this instance have a class of .hov
-        $('body').popover({selector: '.day:not(.inactive)', html: true, placement:'bottom', container:'.wrapper', content: function() {
-            var year = $(this).attr('data-year');
-            var month = $(this).attr('data-month');
-            var day = $(this).attr('data-day');
-            var date = new Date(year, month, day);
+//         // enable popovers - all of mine in this instance have a class of .hov
+//         $('body').popover({selector: '.day:not(.inactive)', html: true, placement:'bottom', container:'.wrapper', content: function() {
+//             var year = $(this).attr('data-year');
+//             var month = $(this).attr('data-month');
+//             var day = $(this).attr('data-day');
+//             var date = new Date(year, month, day);
 
-            return  '<form class="new-appointment-form">' +
-                        '<div class="clearfix">' +
-                            '<label class="control-label appointment-type-label">Stylist</label>' +
-                            '<div class="appointment-stylist-selection">' +
-                                '<select class="selectpicker-popover" id="appointment-stylist" data-width="100%">' +
-                                    '<option>Alexa Johnson</option>' +
-                                    '<option>Amy Brown</option>' +
-                                    '<option>Amanda Rinkey</option>' +
-                                    '<option>Cris Rio</option>' +
-                                    '<option>Shoni Summers</option>' +
-                                '</select>' +
-                            '</div>' +
-                        '</div>' +
-                        '<div class="clearfix">' +
-                            '<label class="control-label appointment-type-label">Type</label>' +
-                            '<div class="appointment-type-selection">' +
-                                '<select class="selectpicker-popover" id="appointment-type" data-width="100%">' +
-                                    '<option>Color</option>' +
-                                    '<option>Cut</option>' +
-                                    '<option>Perm</option>' +
-                                    '<option>Hilight</option>' +
-                                    '<option>Shave</option>' +
-                                '</select>' +
-                            '</div>' +
-                        '</div>' +
-                         '<div class="clearfix">' +
-                            '<label class="control-label appointment-type-label">Type</label>' +
-                            '<div class="appointment-type-selection">' +
-                                '<select class="selectpicker-popover" id="appointment-type" data-width="100%">' +
-                                    '<option>12:00pm</option>' +
-                                    '<option>1:00pm</option>' +
-                                    '<option>2:00pm</option>' +
-                                    '<option>3:00pm</option>' +
-                                    '<option>4:00pm</option>' +
-                                '</select>' +
-                            '</div>' +
-                        '</div>' +                       
-                        '<div class="form-group">' +
-                            '<div class="submit-button">' +
-                                '<button type="submit" class="btn btn-primary">Done</button>' +
-                            '</div>' +
-                        '</div>' +
-                    '</form>';
-        }});
-        
-        // only allow 1 popover at a time
-        // all my popovers hav
-        $('body').on('click', '.day:not(.inactive)', function(e) {
-            // don't fall through
-            e.stopPropagation();
-            //$('.day:not(.inactive)').not(this).popover('hide');
-        });
+//             return  '<form class="new-appointment-form">' +
+//                         '<div class="clearfix">' +
+//                             '<label class="control-label appointment-type-label">Stylist</label>' +
+//                             '<div class="appointment-stylist-selection">' +
+//                                 '<select class="selectpicker-popover" id="appointment-stylist" data-width="100%">' +
+//                                     '<option>Alexa Johnson</option>' +
+//                                     '<option>Amy Brown</option>' +
+//                                     '<option>Amanda Rinkey</option>' +
+//                                     '<option>Cris Rio</option>' +
+//                                     '<option>Shoni Summers</option>' +
+//                                 '</select>' +
+//                             '</div>' +
+//                         '</div>' +
+//                         '<div class="clearfix">' +
+//                             '<label class="control-label appointment-type-label">Type</label>' +
+//                             '<div class="appointment-type-selection">' +
+//                                 '<select class="selectpicker-popover" id="appointment-type" data-width="100%">' +
+//                                     '<option>Color</option>' +
+//                                     '<option>Cut</option>' +
+//                                     '<option>Perm</option>' +
+//                                     '<option>Hilight</option>' +
+//                                     '<option>Shave</option>' +
+//                                 '</select>' +
+//                             '</div>' +
+//                         '</div>' +
+//                          '<div class="clearfix">' +
+//                             '<label class="control-label appointment-type-label">Type</label>' +
+//                             '<div class="appointment-type-selection">' +
+//                                 '<select class="selectpicker-popover" id="appointment-type" data-width="100%">' +
+//                                     '<option>12:00pm</option>' +
+//                                     '<option>1:00pm</option>' +
+//                                     '<option>2:00pm</option>' +
+//                                     '<option>3:00pm</option>' +
+//                                     '<option>4:00pm</option>' +
+//                                 '</select>' +
+//                             '</div>' +
+//                         '</div>' +
+//                         '<div class="form-group">' +
+//                             '<div class="submit-button">' +
+//                                 '<button type="submit" class="btn btn-primary">Done</button>' +
+//                             '</div>' +
+//                         '</div>' +
+//                     '</form>';
+//         }});
 
-        $('.selectpicker').selectpicker({container: 'body'});
+//         // only allow 1 popover at a time
+//         // all my popovers hav
+//         $('body').on('click', '.day:not(.inactive)', function(e) {
+//             // don't fall through
+//             e.stopPropagation();
+//             //$('.day:not(.inactive)').not(this).popover('hide');
+//         });
 
-        $('body').on('shown.bs.popover', '.day:not(.inactive)', function () {
-            $('.selectpicker-popover').selectpicker();
-        });
-    };
+//         $('.selectpicker').selectpicker({container: 'body'});
+
+//         $('body').on('shown.bs.popover', '.day:not(.inactive)', function () {
+//             $('.selectpicker-popover').selectpicker();
+//         });
+//     };
+// });
+
+scheduleDirectives.directive('calendarPopover', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attr) {
+            elm.popover({html: true, placement:'bottom', container:'.wrapper', content: function() {
+                var year = $(this).attr('data-year');
+                var month = $(this).attr('data-month');
+                var day = $(this).attr('data-day');
+                var date = new Date(year, month, day);
+
+                return  '<form class="new-appointment-form">' +
+                            '<div class="clearfix">' +
+                                '<label class="control-label appointment-type-label">Stylist</label>' +
+                                '<div class="appointment-stylist-selection">' +
+                                    '<select class="selectpicker-popover" id="appointment-stylist" data-width="100%">' +
+                                        '<option>Alexa Johnson</option>' +
+                                        '<option>Amy Brown</option>' +
+                                        '<option>Amanda Rinkey</option>' +
+                                        '<option>Cris Rio</option>' +
+                                        '<option>Shoni Summers</option>' +
+                                    '</select>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="clearfix">' +
+                                '<label class="control-label appointment-type-label">Type</label>' +
+                                '<div class="appointment-type-selection">' +
+                                    '<select class="selectpicker-popover" id="appointment-type" data-width="100%">' +
+                                        '<option>Color</option>' +
+                                        '<option>Cut</option>' +
+                                        '<option>Perm</option>' +
+                                        '<option>Hilight</option>' +
+                                        '<option>Shave</option>' +
+                                    '</select>' +
+                                '</div>' +
+                            '</div>' +
+                             '<div class="clearfix">' +
+                                '<label class="control-label appointment-type-label">Type</label>' +
+                                '<div class="appointment-type-selection">' +
+                                    '<select class="selectpicker-popover" id="appointment-type" data-width="100%">' +
+                                        '<option>12:00pm</option>' +
+                                        '<option>1:00pm</option>' +
+                                        '<option>2:00pm</option>' +
+                                        '<option>3:00pm</option>' +
+                                        '<option>4:00pm</option>' +
+                                    '</select>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="form-group">' +
+                                '<div class="submit-button">' +
+                                    '<button type="submit" class="btn btn-primary">Done</button>' +
+                                '</div>' +
+                            '</div>' +
+                        '</form>';
+            }});
+        }
+    }
 });
 
+scheduleDirectives.directive('selectpicker', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attr) {
+            elm.selectpicker({container: 'body'});
+        }
+    }
+});
