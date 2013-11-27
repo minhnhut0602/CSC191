5f1403d98a14c1379cb6ea11d2e35e1a3c8dab57
scottlivingston <s.livingst@gmail.com>
2013-11-26 16:14:40 -0800

    broken selecter thingy

--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -66,57 +66,42 @@ scheduleDirectives.directive('calendarPopover', function() {
             elm.on('shown.bs.popover', function () {
                 $('.selectpicker-popover').selectpicker();
             });
-            $('.day:not(.inactive)').click(function(e) {
-                $('.day:not(.inactive)').not($(this)).popover('hide');
-                console.log("day event click");
-                e.stopPropagation();
-            });
-            $('body').on('click', '.popover', function(e) {
-                // $('.day:not(.inactive)').not($(this)).popover('hide');
-                // e.stopPropagation();
-            });
-            $(document).on('click', '.selectpicker-popover', function(e) {
-                // $('.day:not(.inactive)').not($(this)).popover('hide');
-                console.log(".select-popover event click");
-                e.stopPropagation();
-            });
-            $('body').click(function() {
-               $('.day:not(.inactive)').popover('hide');
-               console.log("body click event");
-            });
         }
     }
 });
 
-scheduleDirectives.directive('selectpicker', function() {
+scheduleDirectives.directive('stylistselect', function($http, $log) {
     return {
-        restrict: 'A',
-        template:   '<select class="selectpicker" multiple data-live-search="true" data-width="40%" >'+
-                        '<option ng-repeat="staff in allStaff" >{{staff.name}}</option>'+
-                    '</select>',
+        restrict: 'E',
+        template: '<select ng-model="selectedItem" multiple ng-options="value.id as value.name for (key, value) in selects" data-style="btn-primary" bs-select></select>',
+        replace: true,
         controller: function($scope, $http) {
-            $http.get('json/stylists.json').success(function(data) {
-                $scope.allStaff = data;
-            });
+
         },
         link: function(scope, elm, attr) {
-            scope.$watch('allStaff', function(newValue, oldValue, scope) {
-                console.log(scope.allStaff);
-                console.log(" staff changed");
-                elm.selectpicker({container: 'body'});
-            }), true;
+            // elm.selectpicker({container: 'body'});
+            scope.selects = [];
+            $http.get('json/stylists.json').success(function(data) {
+                scope.selects = data;
+            });
+            console.log(scope.selects);
+
+            // scope.$watch('allStaff', function(newValue, oldValue, scope) {
+            //     scope.allStaff = newValue;
+            //     elm.selectpicker('refresh');
+            // }, true);
         }
     }
 });
-scheduleDirectives.directive('clienttakeoff', function() {
+scheduleDirectives.directive('appointmentgetter', function($http) {
     return {
         restrict: 'A',
         template:   '<div ng-repeat="appointment in appointments">'+
-                        '<div class="alert alert-{{appointment.myColor}} " ng-click="calendar(appointment.startTime.getFullYear(),appointment.startTime.getMonth(),appointment.startTime.getDate())" >You have an appointment with <span cockeyes stylisturl="{{appointment.stylist}}"></span> at {{appointment.startTime.toLocaleTimeString() }}   {{appointment.myColor}}' +
+                        '<div class="alert alert-{{appointment.myColor}} " ng-click="calendar(appointment.startTime.getFullYear(),appointment.startTime.getMonth(),appointment.startTime.getDate())" >You have an appointment with <stylistname stylisturl="appointment.stylist"/> at {{appointment.startTime.toLocaleTimeString()}}' +
                         '</div>'+
                     '</div>',
-        controller: function($scope, $http) {
-            var config = {headers:  {
+        link: function(scope, elm, attr) {
+            var config = { headers:  {
                     'authToken': readCookie("myAccessToken"),
                     'Content-Type': 'application/json',
                     'Cache-Control': 'no-cache',
@@ -124,7 +109,7 @@ scheduleDirectives.directive('clienttakeoff', function() {
                 }
             };
             $http.get('http://home.joubin.me/salon-scheduler-api/appointments', config).success(function(data) {
-                $scope.appointments = [];
+                scope.appointments = [];
 
                 for (var something in data){
                     var tempAppointment = {};
@@ -146,27 +131,30 @@ scheduleDirectives.directive('clienttakeoff', function() {
                     }
                     for (var link in data[something].links) {
                         if (data[something].links[link].rel === "stylist") {
-                            console.log(data[something].links[link].href);
                             tempAppointment.stylist = data[something].links[link].href;
                         }
                     }
-                    $scope.appointments.push(tempAppointment);
+                    scope.appointments.push(tempAppointment);
                 }
-                console.log($scope.appointments);
+                console.log(scope.appointments);
             });
+        },
+        controller: function($scope, $http) {
+
         }
     };
 });
 
 
-scheduleDirectives.directive('cockeyes', function() {
+scheduleDirectives.directive('stylistname', function($http) {
     return {
-        restrict: 'A',
+        restrict: 'E',
         scope: {
-            stylisturl: '@'
+            stylisturl: '='
         },
-        template: '{{stylist.firstname}}',
-        controller: function($scope, $http) {
+        replace: true,
+        template: '<span>{{stylist.firstName}}</span>',
+        link: function(scope, elm, attr) {
             var config = {headers:  {
                     'authToken': readCookie("myAccessToken"),
                     'Content-Type': 'application/json',
@@ -174,11 +162,119 @@ scheduleDirectives.directive('cockeyes', function() {
                     // 'debug': 'asd'
                 }
             };
-            console.log($scope);
-            console.log($scope.stylisturl);
-            $http.get($scope.stylisturl, config).success(function(data){
-                $scope.stlyist = data;
+            $http.get(scope.stylisturl, config).success(function(data){
+                scope.stylist = data;
             });
+        },
+        controller: function($scope) {
+        }
+    };
+});
+
+
+
+scheduleDirectives.directive('calendar', function() {
+    return {
+        restrict: 'E',
+        template: '',
+        scope: {
+            clickable: '='
+        },
+        link: function() {
+
+        },
+        controller: function($scope) {
+            var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+            $scope.calendar = function(year, monthIndex, day) {
+                if (year === undefined &&
+                    month === undefined &&
+                    day === undefined) {
+                    var d = new Date();
+                year = d.getFullYear();
+                monthIndex = d.getMonth();
+                day = d.getDate();
+            }
+
+            var date = new Date(year, monthIndex, 1);
+            var current = new Date(year, monthIndex, day);
+            var month = {};
+            var weeks = [];
+
+            month.name = monthNames[monthIndex];
+            month.year = date.getFullYear();
+            month.number = date.getMonth();
+
+                // loop for weeks
+                for (var j=0 ; date.getMonth() === monthIndex ; j++) {
+                    //loop for days
+                    var weekdays = [];
+                    for (var k=0 ; date.getMonth() === monthIndex ; k++){
+                        // console.log(scope.date);
+
+                        if (date.getDay() === 0 && k !== 0) {
+                            break;
+                        }
+                        // loop until you see the day go to 0 then jump out OR the month changes
+                        var dayNumber = date.getDate();
+                        var dayName   = dayNames[date.getDay()];
+                        var dayClass  = "";
+                        if (date.getTime() < current.getTime()) {
+                            dayClass = "inactive";
+                        }
+                        if (date.getDate()  === current.getDate() &&
+                            date.getMonth() === current.getMonth() &&
+                            date.getYear()  === current.getYear()) {
+                            dayClass = "today";
+                    }
+                    weekdays.push({number: dayNumber, name: dayName, class:dayClass});
+
+                        // increment the date
+                        date.setDate(date.getDate()+1);
+                    }
+                    weeks.push(weekdays);
+                }
+                // month.weeks = weeks;
+                // console.log(month);
+                month.weeks = weeks;
+                $scope.month = month;
+            };
+            var d = new Date();
+            $scope.calendar(d.getFullYear(), d.getMonth(), d.getDate());
+            $scope.dayNow = d.getDate();
+            $scope.monthNow = {"num": d.getMonth(), "name":monthNames[d.getMonth()]};
+            $scope.yearNow = d.getFullYear();
+            $scope.monthNames = monthNames;
+        }
+    };
+});
+
+scheduleDirectives.directive('modal', function() {
+    return {
+        restrict: 'E',
+        template:   '<div class="modal fade">'+
+                        '<div class="modal-dialog">'+
+                            '<div class="modal-content">'+
+                                '<div class="modal-header">'+
+                                    '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'+
+                                    '<h4 class="modal-title">Modal title</h4>'+
+                                '</div>'+
+                                '<div class="modal-body">'+
+                                    '<p>One fine body&hellip;</p>'+
+                                '</div>'+
+                                '<div class="modal-footer">'+
+                                    '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'+
+                                    '<button type="button" class="btn btn-primary">Save changes</button>'+
+                                '</div>'+
+                            '</div><!-- /.modal-content -->'+
+                        '</div><!-- /.modal-dialog -->'+
+                    '</div><!-- /.modal -->',
+        scope: {
+            availability: '=',
+            day: '='
+        },
+        controller: function($scope) {
+
         }
     };
 });
