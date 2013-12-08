36ef44fb77d2166869d886b718bef0a87824dcec
scottlivingston <s.livingst@gmail.com>
2013-12-07 19:59:07 -0800

    i dont even know anymnore

--- frontend/js/availability.js
+++ frontend/js/availability.js
@@ -2,14 +2,29 @@ var availabilityModule = angular.module('availabilityModule', []);
 
 availabilityModule.controller('TimepickerDemoCtrl', function TimepickerDemoCtrl($scope) {
     $scope.mytime = new Date();
+    while ($scope.mytime.getDay() != 1) {//monday
+        $scope.setDate($scope.getDate()-1);
+    }
+    $scope.mytime.setMinutes(0);
+    $scope.mytime.setSeconds(0);
+    $scope.mytime.setMilliseconds(0);
+
+    $scope.monday    = $scope.mytime;
+
+    $scope.tuesday   = $scope.monday;
+    $scope.tuesday.setDate($scope.tuesday.getDate()+1);
+
+    $scope.wednesday = $scope.tuesday;
+    $scope.wednesday.setDate($scope.wednesday.getDate()+1);
+
+    $scope.thursday  = $scope.wednesday;
+    $scope.thursday.setDate($scope.thursday.getDate()+1);
+
+    $scope.friday    = $scope.thursday;
+    $scope.friday.setDate($scope.friday.getDate()+1);
 
     $scope.hstep = 1;
     $scope.mstep = 15;
-
-    $scope.options = {
-        hstep: [1, 2, 3],
-        mstep: [1, 5, 10, 15, 25, 30]
-    };
 
     $scope.ismeridian = true;
     $scope.toggleMode = function() {
